a01873f883d7f732dc8855c55927587c0e12ca50
Rodrigo Coelho <rodrigo.coelho@iisb.fraunhofer.de>
2025-03-13 13:37:37 +0100

    Updated configs

--- main_tune.py
+++ main_tune.py
@@ -12,7 +12,7 @@ from cleanqrl_utils.train import train_agent
 
 if __name__ == "__main__":
     # Specify the path to the config file
-    config_path = "configs/tune/reinforce_quantum_frozenlake.yaml"
+    config_path = "configs/benchmarks/ppo_pendulum_classical.yaml"
 
     # Load the config file
     with open(config_path) as f:
